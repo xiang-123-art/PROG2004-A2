7096b46102ece53717136d4aae603fb243896afc
xianghu <1599474399@qq.com>
2025-12-01 17:13:36 +0800

    Add Person.java

--- src/Person.java
+++ src/Person.java
@@ -15,4 +15,5 @@ public abstract class Person {
     public void   setName(String name){this.name=name;}
     public int    getAge()           { return age;  }
     public void   setAge(int age)    { this.age=age;}
+    
 }
